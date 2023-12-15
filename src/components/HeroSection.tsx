f8aef064acea174f24e661158322092f82dd6751
Joe Toeniskoetter <jtoeniskoetter.jt@gmail.com>
2023-12-15 09:07:41 -0600

    updates to Hero component styling

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,7 +7,9 @@ export const HeroSection: React.FC = () => {
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           One stop shop for{" "}
-          <span className="text-mrts-orange">Chimney Sweep Professionals</span>
+          <span className="text-mrts-orange font-['Lobster']">
+            Chimney Sweep Professionals
+          </span>
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
           Providing the tools for your business to succeed. Scheduling,
@@ -19,6 +21,18 @@ export const HeroSection: React.FC = () => {
             className="bg-mrts-orange inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
           >
             Get Started
+            <svg
+              className="ml-2 w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </Link>
         </div>
       </div>
