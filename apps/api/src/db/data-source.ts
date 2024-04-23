721b562f9c62571087ad54ac080a2bd16275ca4e
Joe Toeniskoetter <jtoeniskoetter.jt@gmail.com>
2024-04-23 16:48:25 -0500

    remove log

--- apps/api/src/db/data-source.ts
+++ apps/api/src/db/data-source.ts
@@ -2,7 +2,6 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-console.log({ databaseUrl: process.env.DATABASE_URL });
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URI,
